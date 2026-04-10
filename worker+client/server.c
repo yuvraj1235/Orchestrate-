26b5ca018fb6f946a67c5b69d8152a34e3585c82
yuvraj1235 <reignitedyuvraj@gmail.com>
2026-04-10 20:00:59 +0530

    corectted server

--- worker+client/server.c
+++ worker+client/server.c
@@ -8,22 +8,11 @@
 #include <pthread.h>
 #include <fcntl.h>
 #include <stdint.h>
-
-using namespace std;
+#include <errno.h>
 
 #define UDP_PORT 9091
 #define TCP_PORT 9090
 #define BUFFER_SIZE 1024
-#define MAX_PEERS 10
-
-typedef struct {
-    char ip[INET_ADDRSTRLEN];
-    double load;
-} Peer;
-
-Peer routing_table[MAX_PEERS];
-int peer_count = 0;
-pthread_mutex_t table_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 double get_system_load() {
     double load[1];
@@ -35,6 +24,11 @@ double get_system_load() {
 
 void *broadcast_load(void *arg) {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("UDP socket failed");
+        return NULL;
+    }
+
     int broadcast = 1;
     setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
 
@@ -46,45 +40,104 @@ void *broadcast_load(void *arg) {
 
     while (1) {
         double current_load = get_system_load();
-        sendto(sock, &current_load, sizeof(current_load), 0, (struct sockaddr *)&addr, sizeof(addr));
+        sendto(sock, &current_load, sizeof(current_load), 0,
+               (struct sockaddr *)&addr, sizeof(addr));
         sleep(2);
     }
     return NULL;
 }
 
+// Helper: read exactly N bytes
+int read_full(int fd, void *buf, size_t size) {
+    size_t total = 0;
+    while (total < size) {
+        int n = read(fd, (char*)buf + total, size - total);
+        if (n <= 0) return -1;
+        total += n;
+    }
+    return 0;
+}
+
 void *worker_server(void *arg) {
     int srv_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (srv_fd < 0) {
+        perror("TCP socket failed");
+        return NULL;
+    }
+
     int opt = 1;
     setsockopt(srv_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    struct sockaddr_in addr = {AF_INET, htons(TCP_PORT), INADDR_ANY};
-    bind(srv_fd, (struct sockaddr *)&addr, sizeof(addr));
-    listen(srv_fd, 5);
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(TCP_PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-    char buffer[BUFFER_SIZE];
+    if (bind(srv_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Bind failed");
+        return NULL;
+    }
+
+    if (listen(srv_fd, 5) < 0) {
+        perror("Listen failed");
+        return NULL;
+    }
+
+    printf("Worker active on port %d...\n", TCP_PORT);
+
     while (1) {
         int cli = accept(srv_fd, NULL, NULL);
-        int64_t f_size = 0;
-        
-        // Read 8-byte file size
-        read(cli, &f_size, sizeof(f_size));
+        if (cli < 0) {
+            perror("Accept failed");
+            continue;
+        }
 
-        int fd = open("incoming_task.c", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        int64_t f_size;
+
+        // FIX: safe read
+        if (read_full(cli, &f_size, sizeof(f_size)) < 0) {
+            close(cli);
+            continue;
+        }
+
+        // Unique filenames (avoid collision)
+        char src_file[64], bin_file[64];
+        sprintf(src_file, "task_%d.c", getpid());
+        sprintf(bin_file, "task_%d", getpid());
+
+        int fd = open(src_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (fd < 0) {
+            perror("File open failed");
+            close(cli);
+            continue;
+        }
+
+        char buffer[BUFFER_SIZE];
         int64_t total_received = 0;
+
         while (total_received < f_size) {
             int b = read(cli, buffer, BUFFER_SIZE);
             if (b <= 0) break;
             write(fd, buffer, b);
             total_received += b;
         }
+
         close(fd);
 
-        // Compile and Execute
-        system("gcc incoming_task.c -o task_bin 2> compile_errors.log");
-        FILE *fp = popen("./task_bin 2>&1", "r");
-        
+        // Compile
+        char compile_cmd[128];
+        sprintf(compile_cmd, "gcc %s -o %s 2> compile_errors.log", src_file, bin_file);
+        system(compile_cmd);
+
+        // Execute
+        char exec_cmd[128];
+        sprintf(exec_cmd, "./%s 2>&1", bin_file);
+
+        FILE *fp = popen(exec_cmd, "r");
+
         if (fp == NULL) {
-            const char *err = "Execution failed.\n";
+            char *err = "Execution failed\n";
             send(cli, err, strlen(err), 0);
         } else {
             while (fgets(buffer, BUFFER_SIZE, fp)) {
@@ -92,19 +145,22 @@ void *worker_server(void *arg) {
             }
             pclose(fp);
         }
-        
+
         close(cli);
-        unlink("incoming_task.c");
-        unlink("task_bin");
+
+        unlink(src_file);
+        unlink(bin_file);
     }
 }
 
 int main() {
-    pthread_t t1, t3;
-    pthread_create(&t1, NULL, broadcast_load, NULL);
-    pthread_create(&t3, NULL, worker_server, NULL);
+    pthread_t t1, t2;
 
-    printf("Worker active. Listening for tasks on TCP %d...\n", TCP_PORT);
+    pthread_create(&t1, NULL, broadcast_load, NULL);
+    pthread_create(&t2, NULL, worker_server, NULL);
+
     pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
     return 0;
 }
